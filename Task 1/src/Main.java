05d2ce024875685898797e51f3e05a46b9997188
Artem Martynov <amerta90@mail.ru>
2022-12-19 21:48:21 +0300

    Create Task 1

---
+++ Task 1/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    static double a = 0;
+    static double b = 0;
+
+    public static void main(String[] args) {
+        //todo Вам нужно дополнить методы так, чтобы получился рабочий калькулятор
+        a = inputDouble();
+        b = inputDouble();
+
+        sum();      //Метод суммирования чисел
+        multiply(); //Метод умножения чисел
+        diff();     //Метод вычитания чисел
+        divide();   //Метод деления чисел
+    }
+
+    private static void divide() {
+        //todo дописать логику и печать результата
+    }
+
+    private static void diff() {
+        //todo дописать логику и печать результата
+    }
+
+    private static void multiply() {
+        //todo дописать логику и печать результата
+    }
+
+    private static void sum() {
+        //todo дописать логику и печать результата
+    }
+
+    private static double inputDouble() {
+        //todo дописать логику
+        return 0;
+    }
+}
