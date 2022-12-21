6478613cf46a178c80e09d7c784826310bba18b2
ximik28 <ximik28@tut.by>
2022-12-21 16:26:22 +0300

    выполнены Task 1 и Task 2

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
@@ -15,22 +17,26 @@ public class Main {
 
     private static void divide() {
         //todo дописать логику и печать результата
+        System.out.println("Результат деления чисел: " + (a / b));
     }
 
     private static void diff() {
         //todo дописать логику и печать результата
+        System.out.println("Результат вычитания чисел: " + (a - b));
     }
 
     private static void multiply() {
         //todo дописать логику и печать результата
+        System.out.println("Результат умножения чисел: " + (a * b));
     }
 
     private static void sum() {
         //todo дописать логику и печать результата
+        System.out.println("Результат сложения чисел: " + (a + b));
     }
 
     private static double inputDouble() {
-        //todo дописать логику
-        return 0;
+        System.out.println("Введите любое вещественное число:");
+        return new Scanner(System.in).nextDouble();
     }
 }
--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
+    static int a;
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+
+        inputNum();
+        inspectNum();
+    }
+
+    static int inputNum() {
+        System.out.println("Введите целое число:");
+        a = new Scanner(System.in).nextInt();
+        return a;
+    }
+    static void inspectNum() {
+        boolean isRes;
+        if (a % 2 == 0) {
+            isRes = true;
+        } else {
+            isRes = false;
+        }
+        System.out.println(isRes);
     }
 }
